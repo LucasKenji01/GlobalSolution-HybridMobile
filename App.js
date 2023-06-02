5068d8598a0c425a70297808ed1699eff8031ea3
Lucas Kenji <rm94233@fiap.com.br>
2023-06-02 20:16:13 -0300

    Login estilizado

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './views/Login';
+import CadastroFuncionarioScreen from './views/CadastroFuncionario';
+import HomeScreen from './views/Home';
+import CadastroUsuarioScreen from './views/CadastroUsuario';
+import DoacaoScreen from './views/Doacao';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen">
+        <Stack.Screen name="Login" component={LoginScreen} options={{
+          title: '',
+          headerTransparent: true,
+          headerShown: false
+        }}/>
+        <Stack.Screen name="CadastroFuncionario" component={CadastroFuncionarioScreen} options={{
+          title: '',
+          headerTransparent: true,
+          headerShown: false
+        }}/>
+        <Stack.Screen name="Home" component={HomeScreen} options={{
+          title: '',
+          headerTransparent: true,
+          headerShown: false
+        }}/>
+        <Stack.Screen name="CadastroUsuario" component={CadastroUsuarioScreen} options={{
+          title: '',
+          headerTransparent: true,
+          headerShown: false
+        }}/>
+        <Stack.Screen name="Doacao" component={DoacaoScreen} options={{
+          title: '',
+          headerTransparent: true,
+          headerShown: false
+        }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ views/Login.js
@@ -0,0 +1,141 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image
+} from "react-native";
+import {
+  useFonts,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  Rubik_700Bold,
+} from "@expo-google-fonts/rubik";
+
+export default function Login() {
+  const [fontLoaded] = useFonts({
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_600SemiBold,
+    Rubik_700Bold,
+  });
+
+  if (!fontLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Login</Text>
+      <View style={styles.linha} />
+      <View style={styles.forms}>
+        <Text style={styles.textoEmail}>Email</Text>
+        <TextInput style={styles.input} />
+        <Text style={styles.textoSenha}>Senha</Text>
+        <TextInput style={styles.input} />
+      </View>
+      <View style={styles.containerBtn}>
+        <TouchableOpacity style={styles.btn}>
+            <Text style={styles.textoBtn}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.separator}>
+        <View style={styles.linhaMenor}/>
+        <Image style={styles.image} source={require('../assets/Garfo-e-faca.png')}/>
+        <View style={styles.linhaMenor}/>
+      </View>
+      <View style={styles.containerCadastrese}>
+        <Text style={styles.ntc}>Não tem cadastro?</Text>
+        <TouchableOpacity style={styles.btn}>
+            <Text style={styles.textoBtn}>Cadastre-se</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F7F7F7",
+    marginTop: 60,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  titulo: {
+    marginTop: 20,
+    fontSize: 36,
+    fontFamily: "Rubik_700Bold",
+  },
+  linha: {
+    width: 330,
+    height: 4,
+    backgroundColor: "#53CF48",
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  forms: {
+    marginTop: 65,
+    marginLeft: 15,
+  },
+  textoEmail: {
+    fontSize: 16,
+    fontFamily: "Rubik_400Regular",
+  },
+  textoSenha: {
+    fontSize: 16,
+    fontFamily: "Rubik_400Regular",
+    marginTop: 40,
+  },
+  input: {
+    marginTop: 5,
+    width: 300,
+    height: 40,
+    backgroundColor: "#FFFFFF",
+    borderWidth: 1,
+    borderRadius: 4,
+  },
+  containerBtn: {
+    marginTop: 40,
+    alignItems: "center",
+  },
+  btn: {
+    width: 180,
+    height: 50,
+    backgroundColor: "#53CF48",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 6,
+  },
+  textoBtn: {
+    fontSize: 20,
+    fontFamily: 'Rubik_600SemiBold',
+  },
+  separator: {
+    flexDirection: 'row',
+    marginTop: 60,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  linhaMenor: {
+    width: 150,
+    height: 4,
+    backgroundColor: "#53CF48",
+    borderRadius: 5,
+  },
+  image: {
+    marginLeft: 4,
+    marginRight: 4,
+  },
+  containerCadastrese: {
+    marginTop: 60,
+    alignItems: "center",
+  },
+  ntc: {
+    fontSize: 16,
+    fontFamily: 'Rubik_500Medium',
+    marginBottom: 40,
+  }
+});
